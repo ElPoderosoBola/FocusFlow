887b01587e8a60ccc5cdfe3b79482a4722d68472
Rodrigo-Daw <rodrigo.garpez@gmail.com>
2026-04-25 08:55:04 +0200

    Proyecto actualizado

--- FocusFlow/MainPage.xaml.cs
+++ FocusFlow/MainPage.xaml.cs
@@ -4,31 +4,18 @@ namespace FocusFlow
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         private readonly MainViewModel _viewModel;
 
         public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
 
-            // Asigna el ViewModel a la vista para el enlace de datos (Binding).
+            // Enchufamos el Titiritero a la Pantalla
             _viewModel = viewModel;
             BindingContext = _viewModel;
 
-            // Inicializa el ViewModel cuando la página ya está preparada.
+            // Cargamos los datos de la base de datos SQLite al arrancar
             _ = _viewModel.InitializeAsync();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
