933df0543dfec1407fbcb2d99f4823115b1c39a3
Guillermo García López <garcialopezg999@gmail.com>
2026-05-02 20:16:55 +0200

    Arreglado el dolor visual de Android

--- FocusFlow/Platforms/Android/MainActivity.cs
+++ FocusFlow/Platforms/Android/MainActivity.cs
@@ -1,11 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
-namespace FocusFlow
+namespace FocusFlow;
+
+// Fíjate que aquí ya pone "@style/Maui.SplashTheme" limpio, sin cosas raras
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
+        base.OnCreate(savedInstanceState);
+
+        // 🪄 ¡MAGIA PARA LAS BARRAS!
+        // Pintamos el techo (Status Bar) y el suelo (Navigation Bar) de azul oscuro
+        var azulOscuro = Android.Graphics.Color.ParseColor("#162533");
+        Window?.SetStatusBarColor(azulOscuro);
+        Window?.SetNavigationBarColor(azulOscuro);
     }
 }
